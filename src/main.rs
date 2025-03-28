f5cecf92323eaf8ab93a45bbf537873fc7136ae0
ichiragkumar <chiragroy3030@gmail.com>
2025-03-28 11:30:18 +0530

    learning rust basisc

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("hello rust , welcome to world of rusty!");
+
+
+    // variable in rust
+    let x : i32 = 10;
+    let y : i32 = 20;
+
+    // variable in rust
+    let x1 : i32 = 10;
+    let y2 : i32 = 20;
+}
+
+
