cba3a1ad13536bf04e7f9c55c3a06abbed05131b
ichiragkumar <chiragroy3030@gmail.com>
2025-03-31 07:40:55 +0530

    conditional statement and rust operation is rust

--- src/main.rs
+++ src/main.rs
@@ -134,9 +134,7 @@ fn main() {
 
 
    // loops in rust
-   for i in 0..10{
-       print!("i is : {} \n", i);
-   }
+   
 
    // print even number in rust , in given range
    let r1: i32 = 0;
@@ -158,16 +156,153 @@ fn main() {
         print!("Prime number is : {} \n", i);
     }
    }
-   
+
 
    for i in (p1r + 2)..(p2r -2 ) {
     if i % 2 !=0 {
         print!("----------");
         println!("Prime number is: {}", i);
     }
-}
 
 
 }
 
 
+    
+
+
+    let is_logged_in : bool = true;
+
+    let is_user_paid : bool = true;
+
+    if is_logged_in {
+        print!(" User is logged in");
+    }else if is_user_paid {
+        print!(" User has paid");
+    }
+
+    if is_logged_in && is_user_paid {
+        print!(" User is logged in and paid");
+    }
+
+
+    // lops in go lnag
+    let mut counter : i32 = 0;
+
+    loop {
+        print!("counter is : {} \n", counter);
+        counter = counter + 1;
+        if counter == 10 {
+            break;
+        }
+    }   
+
+    // for loop in rust
+    for i in 0..10{
+        print!("i is : {} \n", i);
+    }
+
+    for i in 0..10{
+        print!("i is : {} \n", i);
+    }
+
+
+    // while loop
+
+
+    // iterate over
+    // array
+    // map
+    // string
+
+
+    let sentence = String::from("Hello rusty, welcome to world of rust");
+
+    let first_word_from = get_first_word(&sentence);
+    print!("First word from sentence is : {}", first_word_from);
+
+    let first_letter_from = get_first_letter(&sentence);
+    print!("First letter from sentence is : {}", first_letter_from);
+
+    let return_asked_index = get_index_of(&sentence, 2);
+    print!("Index of rust in sentence is : {}", return_asked_index);
+
+
+    // learn why we use used &str here [moved and boroow up]
+    // and then updated the function call with &str
+
+    // let sentence = String::from("Hello rusty, welcome to world of rust");
+
+    // let first_word_from = get_first_word(&sentence);
+    // print!("First word from sentence is : {}", first_word_from);
+
+    // let first_letter_from = get_first_letter(&sentence);
+    // print!("First letter from sentence is : {}", first_letter_from);
+
+
+    // sum of two number in rust
+    let sum1:i32 = 10;
+    let sum2:i32 = 20;
+
+    let ans = sum_of_two_numbers(sum1, sum2);
+    print!("Sum of two numbers is : {}", ans);
+
+    // sum of n number_n is
+    let result:i128 = sum_of_numbers(10000);
+    print!("Sum of n number is : {}", result);
+
+
+
+
+
+
+}
+
+
+fn get_first_word(sentence: &str) -> String {
+    let mut ans = String::new();
+    
+    for char in sentence.chars() {
+        if char == ' ' {
+            break;
+        }
+        ans.push(char);
+    }
+    
+    return ans
+}
+
+
+fn get_first_letter(sentence: &str) -> String {
+
+
+    
+    return sentence.chars().nth(0).unwrap().to_string();
+}
+
+fn get_index_of(sentence: &str, index: usize) -> String {
+    if let Some(c) = sentence.chars().nth(index) {
+        return c.to_string();
+    }
+    return String::from("chirag"); 
+}
+
+
+fn sum_of_two_numbers(num1:i32, num2:i32) -> i32{
+    return num1 + num2;
+}
+
+fn sum_of_numbers(index:i128) -> i128{
+    // let mut total_count_is : i128 = 0;
+    // for i in 0..index{
+    //     total_count_is = total_count_is + i;
+    // }
+    // return total_count_is;
+
+
+    // ideal way
+    // (n * (n - 1)) / 2
+
+    return  (index * (index - 1)) / 2;
+
+}
