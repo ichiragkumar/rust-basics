f31d666b9a3b1d94d522649a0cbec2ee1b16ddd7
ichiragkumar <chiragroy3030@gmail.com>
2025-04-05 00:11:29 +0530

    implement rust struct with impl and debug/traits/for

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,28 @@ struct User {
     sign_in_count:u64,
 }
 
+// implementation in rust structs
+
+
+struct Rect {
+    width : u32,
+    height: u32,
+}
+
+impl  Rect {
+
+    fn area(&self) -> u32{
+        return self.height *  self.width;
+    }   
+
+    fn perimeter(&self) -> u32 {
+        return  2 * ( self.height + self.width);
+    }
+
+
+
+}
+
 fn main(){
     let name =  String::from("chirag kumar");
     let email = String::from("chireagory3030@gmail.com");
@@ -15,5 +37,15 @@ fn main(){
         email
     };
 
-    print!("user name is {} and his email is {}", user.username, user.email)
+    print!("user name is {} and his email is {}", user.username, user.email);
+
+    print!("--------------");
+
+    let rect = Rect{
+        width:10,
+        height:100
+    };
+
+    print!("area of rectangle is {}", rect.area());
+    print!("peremeter of reactangle is {}", rect.perimeter());
 }
